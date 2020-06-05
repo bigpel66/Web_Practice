19d6659766d6e85742ddcdb477b1ab82a6f1cbb8
BIGPEL <bigpel66@gmail.com>
2020-06-05 15:36:51 +0900

    2048 finished

--- inflearn/Web Game/2048/2048.js
+++ inflearn/Web Game/2048/2048.js
@@ -4,66 +4,247 @@ const button = document.querySelector('#button');
 
 let isStart = false;
 let dataOnTable = [];
+let formerTable = [];
 
 let isDragStart = false;
 let isDragging = false;
 let startCoord;
 let endCoord;
+let isChanged = false;
 
-const handleRight = () => {
-    const newData = [[], [], [], []];
+const checkWin = () => {
+    let check = false;
 
     dataOnTable.forEach((row, i) => {
-        row.forEach((attr, j) => {});
+        row.forEach((attr, j) => {
+            if (attr === 2048) {
+                check = true;
+            }
+        });
     });
+
+    return check;
+};
+
+const tableCompare = () => {
+    let check = false;
+
+    dataOnTable.forEach((row, i) => {
+        row.forEach((attr, j) => {
+            if (attr !== formerTable[i][j]) {
+                check = true;
+            }
+        });
+    });
+
+    return check;
+};
+
+const makeCopyTableData = () => {
+    formerTable = [];
+
+    dataOnTable.forEach((row, i) => {
+        formerTable.push([]);
+        row.forEach((attr, j) => {
+            formerTable[i].push(attr);
+        });
+    });
+};
+
+const handleRight = () => {
+    let getScore = 0;
+    const newData = [[], [], [], []];
+
+    [0, 1, 2, 3].forEach((rowIndex) => {
+        let currentValue = -1;
+
+        [0, 1, 2, 3].forEach((colIndex) => {
+            if (dataOnTable[rowIndex][colIndex] === 0) {
+                if (
+                    colIndex + 1 === dataOnTable[rowIndex].length &&
+                    currentValue !== -1
+                ) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+                return;
+            }
+
+            if (currentValue === dataOnTable[rowIndex][colIndex]) {
+                currentValue = -1;
+                newData[rowIndex].unshift(dataOnTable[rowIndex][colIndex] * 2);
+                getScore += dataOnTable[rowIndex][colIndex] * 2;
+            } else {
+                if (currentValue !== -1) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+                currentValue = dataOnTable[rowIndex][colIndex];
+                if (
+                    colIndex + 1 === dataOnTable[rowIndex].length &&
+                    currentValue !== -1
+                ) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+            }
+        });
+    });
+
+    score.textContent = Number(score.textContent) + getScore;
+
+    makeCopyTableData();
 
     [0, 1, 2, 3].forEach((fIter) => {
         [0, 1, 2, 3].forEach((sIter) => {
-            dataOnTable[fIter][sIter] = newData[fIter][sIter] || 0;
+            dataOnTable[fIter][3 - sIter] = newData[fIter][sIter] || 0;
         });
     });
+
+    if (tableCompare()) {
+        isChanged = true;
+    }
 };
 
 const handleLeft = () => {
+    let getScore = 0;
     const newData = [[], [], [], []];
 
-    dataOnTable.forEach((row, i) => {
-        row.forEach((attr, j) => {});
+    [0, 1, 2, 3].forEach((rowIndex) => {
+        let currentValue = -1;
+
+        [3, 2, 1, 0].forEach((colIndex) => {
+            if (dataOnTable[rowIndex][colIndex] === 0) {
+                if (colIndex - 1 === -1 && currentValue !== -1) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+                return;
+            }
+
+            if (currentValue === dataOnTable[rowIndex][colIndex]) {
+                currentValue = -1;
+                newData[rowIndex].unshift(dataOnTable[rowIndex][colIndex] * 2);
+                getScore += dataOnTable[rowIndex][colIndex] * 2;
+            } else {
+                if (currentValue !== -1) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+                currentValue = dataOnTable[rowIndex][colIndex];
+                if (colIndex - 1 === -1 && currentValue !== -1) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+            }
+        });
     });
+
+    score.textContent = Number(score.textContent) + getScore;
+
+    makeCopyTableData();
 
     [0, 1, 2, 3].forEach((fIter) => {
         [0, 1, 2, 3].forEach((sIter) => {
             dataOnTable[fIter][sIter] = newData[fIter][sIter] || 0;
         });
     });
+
+    if (tableCompare()) {
+        isChanged = true;
+    }
 };
 
 const handleDown = () => {
+    let getScore = 0;
     const newData = [[], [], [], []];
 
-    dataOnTable.forEach((row, i) => {
-        row.forEach((attr, j) => {});
+    [0, 1, 2, 3].forEach((rowIndex) => {
+        let currentValue = -1;
+
+        [0, 1, 2, 3].forEach((colIndex) => {
+            if (dataOnTable[colIndex][rowIndex] === 0) {
+                if (
+                    colIndex + 1 === dataOnTable[rowIndex].length &&
+                    currentValue !== -1
+                ) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+                return;
+            }
+
+            if (currentValue === dataOnTable[colIndex][rowIndex]) {
+                currentValue = -1;
+                newData[rowIndex].unshift(dataOnTable[colIndex][rowIndex] * 2);
+                getScore += dataOnTable[colIndex][rowIndex] * 2;
+            } else {
+                if (currentValue !== -1) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+                currentValue = dataOnTable[colIndex][rowIndex];
+                if (
+                    colIndex + 1 === dataOnTable[rowIndex].length &&
+                    currentValue !== -1
+                ) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+            }
+        });
     });
+
+    score.textContent = Number(score.textContent) + getScore;
+
+    makeCopyTableData();
 
     [0, 1, 2, 3].forEach((fIter) => {
         [0, 1, 2, 3].forEach((sIter) => {
-            dataOnTable[fIter][sIter] = newData[fIter][sIter] || 0;
+            dataOnTable[3 - fIter][sIter] = newData[sIter][fIter] || 0;
         });
     });
+
+    if (tableCompare()) {
+        isChanged = true;
+    }
 };
 
 const handleUp = () => {
+    let getScore = 0;
     const newData = [[], [], [], []];
 
-    dataOnTable.forEach((row, i) => {
-        row.forEach((attr, j) => {});
+    [0, 1, 2, 3].forEach((rowIndex) => {
+        let currentValue = -1;
+
+        [3, 2, 1, 0].forEach((colIndex) => {
+            if (dataOnTable[colIndex][rowIndex] === 0) {
+                if (colIndex - 1 === -1 && currentValue !== -1) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+                return;
+            }
+
+            if (currentValue === dataOnTable[colIndex][rowIndex]) {
+                currentValue = -1;
+                newData[rowIndex].unshift(dataOnTable[colIndex][rowIndex] * 2);
+                getScore += dataOnTable[colIndex][rowIndex] * 2;
+            } else {
+                if (currentValue !== -1) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+                currentValue = dataOnTable[colIndex][rowIndex];
+                if (colIndex - 1 === -1 && currentValue !== -1) {
+                    newData[rowIndex].unshift(currentValue);
+                }
+            }
+        });
     });
+
+    score.textContent = Number(score.textContent) + getScore;
+
+    makeCopyTableData();
 
     [0, 1, 2, 3].forEach((fIter) => {
         [0, 1, 2, 3].forEach((sIter) => {
-            dataOnTable[fIter][sIter] = newData[fIter][sIter] || 0;
+            dataOnTable[fIter][sIter] = newData[sIter][fIter] || 0;
         });
     });
+
+    if (tableCompare()) {
+        isChanged = true;
+    }
 };
 
 const setDirection = (start, end) => {
@@ -95,6 +276,15 @@ const actionDirection = (direction) => {
         case 'up':
             handleUp();
             break;
+    }
+    dataToTable();
+    if (isChanged) {
+        isChanged = false;
+        numberOnRandomTile();
+    }
+    if (checkWin()) {
+        alert(`Score: ${score.textContent}`);
+        gameInit();
     }
 };
 
@@ -187,7 +377,7 @@ const mouseMoveEvent = (event) => {
 };
 
 const mouseUpEvent = (event) => {
-    if (isDragging) {
+    if (isDragging && isStart) {
         endCoord = [event.clientX, event.clientY];
 
         const direction = setDirection(startCoord, endCoord);
